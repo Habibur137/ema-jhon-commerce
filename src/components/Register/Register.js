97973757ae7f50a843775fbe6e02e55b34e790da
Habibur137 <infernohabib@gmail.com>
2022-04-10 21:57:34 +0600

    event handler added for every input

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const emailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const passwordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+  const confirmPasswordBlur = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+  const createUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("password did not match");
+      return;
+    }
+  };
   return (
     <div className="flex justify-center w-96 mx-auto mt-8 bg-[#f7f7f7] shadow-2xl p-4">
       <div>
         <h1 className="text-2xl mb-4 text-center">Register</h1>
-        <form>
+        <form onSubmit={createUser}>
           <div className="flex flex-col w-full">
             <label className="text-xl" htmlFor="email">
               Email
             </label>
             <input
+              onBlur={emailBlur}
               className="border px-2 py-1 outline-0"
               type="email"
               name="email"
@@ -24,6 +45,7 @@ const Register = () => {
               Password
             </label>
             <input
+              onBlur={passwordBlur}
               className="border px-2 py-1 outline-0"
               type="password"
               name="email"
@@ -36,6 +58,7 @@ const Register = () => {
               Confirm Password
             </label>
             <input
+              onBlur={confirmPasswordBlur}
               className="border px-2 py-1 outline-0"
               type="password"
               name="confirmpassword"
@@ -43,6 +66,7 @@ const Register = () => {
               required
             />
           </div>
+          <p>{error}</p>
           <input
             className="w-full bg-yellow-600 mt-6 px-2 py-1 text-2xl"
             type="submit"
